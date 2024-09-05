655e15c79fe06e59cda6086deee5f48362bed870
user <user@F178>
2024-09-05 10:49:38 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/C130Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class C130Controller : Controller
+    {
+        // GET: C130
+        public ActionResult CalcTax()
+        {
+            return View(new C130Vm());
+        }
+
+        [HttpPost]
+        public ActionResult CalcTax(C130Vm vm)
+        {
+            return View(vm);
+        }
+    }
+}
---
+++ WebApplication1/Models/ViewModels/C130Vm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class C130Vm
+    {
+        [Display(Name ="未稅價")]
+        [Required(ErrorMessage = "{0}必填")]
+        [Range(1,int.MaxValue,ErrorMessage ="{0}必須大於{1}")]
+        public int Price { get; set; }
+
+        public int Tax => (int)Math.Round(Price*0.05,MidpointRounding.AwayFromZero);
+
+        public int Total =>Price +Tax;
+    }
+}
